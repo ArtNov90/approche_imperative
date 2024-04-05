fb893f77d45ae108995b0cf974a2df6741bc8b0c
ArtNov90 <art.novara@gmail.com>
2024-04-05 10:31:38 +0200

    exercice AffichageSuite while 1

--- src/fr/algorithmie/AffichageSuite.java
+++ src/fr/algorithmie/AffichageSuite.java
@@ -4,12 +4,15 @@ public class AffichageSuite {
 
 	public static void main(String[] args) {
 		
-		for( int i = 1; i < 11; i++ ){
-            if (i % 2 != 0) {
-                System.out.println(i);
-            }
-        }
+		
+			int i ;
 			
+			i = 1;
+			while (i<=10) {
+				
+				System.out.println(i);
+				i++;
+			}
 			
 		
 		
