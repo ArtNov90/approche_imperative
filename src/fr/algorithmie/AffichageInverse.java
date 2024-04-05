5f3545a5bc5438d1ea6d1bf6b0af648ed6c17c27
ArtNov90 <art.novara@gmail.com>
2024-04-05 10:53:49 +0200

    exercice AffichageInverse tableau inversé

--- src/fr/algorithmie/AffichageInverse.java
+++ src/fr/algorithmie/AffichageInverse.java
@@ -7,7 +7,7 @@ public class AffichageInverse {
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ; 
 		
 		
-        for (int i = 0; i < array.length; i++) {
+        for (int i = array.length - 1; i>=0 ; i--) {
             System.out.println(array[i]);
         }
 
