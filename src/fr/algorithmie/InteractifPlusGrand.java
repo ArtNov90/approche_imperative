7d955de3801d1c55303629926b18615875f30791
ArtNov90 <art.novara@gmail.com>
2024-04-05 16:42:29 +0200

    EXERCICE INTERACTIF PLUS GRAND

--- src/fr/algorithmie/InteractifPlusGrand.java
+++ src/fr/algorithmie/InteractifPlusGrand.java
@@ -1,9 +1,27 @@
 package fr.algorithmie;
 
+import java.util.Scanner;
+
 public class InteractifPlusGrand {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+
+		int plusGrand = Integer.MIN_VALUE;
+		
+		System.out.println("Veuillez saisir 10 nombres :");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Nombre " + (i + 1) + " : ");
+            int nombre = scanner.nextInt();
+            if (nombre > plusGrand) {
+                plusGrand = nombre; 
+            }
+        }
+        
+        System.out.println("Le plus grand nombre saisi est : " + plusGrand);
+        
+        scanner.close();
 
 	}
 
