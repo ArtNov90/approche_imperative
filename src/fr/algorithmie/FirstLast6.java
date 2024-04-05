d1d07cdadf4f2e4df60af76532eca6270d301d80
ArtNov90 <art.novara@gmail.com>
2024-04-05 14:40:10 +0200

    EXERCICE FIRSTLAST

---
+++ src/fr/algorithmie/FirstLast6.java
@@ -0,0 +1,18 @@
+package fr.algorithmie;
+
+public class FirstLast6 {
+
+	public static void main(String[] args) {
+
+		int[] array = {8, 0, 1, 3, 7, 5};
+		boolean valeurBooleenne;
+		
+		valeurBooleenne = (array.length > 0 && (array[0] == 6 ));
+		
+		System.out.println("La valeur booléenne est : " + valeurBooleenne);
+		
+		
+
+	}
+
+}
