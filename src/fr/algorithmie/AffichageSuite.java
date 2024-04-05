8c96ac9d4a216a81990c04ad7bfd8dbf6e353067
ArtNov90 <art.novara@gmail.com>
2024-04-05 10:18:03 +0200

    exercice AffichageSuite modulo impair

--- src/fr/algorithmie/AffichageSuite.java
+++ src/fr/algorithmie/AffichageSuite.java
@@ -5,7 +5,7 @@ public class AffichageSuite {
 	public static void main(String[] args) {
 		
 		for( int i = 1; i < 11; i++ ){
-            if (i % 2 == 0) {
+            if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
