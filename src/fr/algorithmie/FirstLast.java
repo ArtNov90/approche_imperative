54a5c486a8c49f5b1a0187d8da786915dab020a9
ArtNov90 <art.novara@gmail.com>
2024-04-05 15:01:40 +0200

    EXERCICE FIRSTLAST longueur de tableau égal à 1

---
+++ src/fr/algorithmie/FirstLast.java
@@ -0,0 +1,23 @@
+package fr.algorithmie;
+
+public class FirstLast {
+
+	public static void main(String[] args) {
+		
+		int[] array = {8, 0, 1, 3, 7, 5};
+		
+		
+		for(int i = 0; i < array.length; i++);
+		
+		if(array.length > 1){
+			
+			System.out.println("true");
+			
+		} else {
+			
+			System.out.println("false");
+		}
+
+	}
+
+}
