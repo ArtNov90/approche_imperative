bf1db8c1baf1704ec6c9279a2218993716783d58
ArtNov90 <art.novara@gmail.com>
2024-04-05 11:14:28 +0200

    exercice InversionContenu

--- src/fr/algorithmie/AffichageInverse.java
+++ src/fr/algorithmie/AffichageInverse.java
@@ -6,9 +6,15 @@ public class AffichageInverse {
 		
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ; 
 		
+		 int[] arrayCopy = new int[array.length];
+	        for (int i = 0; i < array.length; i++) {
+	            arrayCopy[i] = array[i];
+	        }
 		
-        for (int i = array.length - 1; i>=0 ; i--) {
-            System.out.println(array[i]);
+		
+	        for (int i = 0; i < arrayCopy.length; i++) {
+	            System.out.println(arrayCopy[i]);
+	            
         }
 
 		
