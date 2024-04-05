666490e7d2565924ee843dc4513224ddb9de94c1
ArtNov90 <art.novara@gmail.com>
2024-04-05 10:17:14 +0200

    exercice AffichageSuite modulo pair

--- src/fr/algorithmie/AffichageSuite.java
+++ src/fr/algorithmie/AffichageSuite.java
@@ -4,9 +4,13 @@ public class AffichageSuite {
 
 	public static void main(String[] args) {
 		
-		for( int i = 1; i < 11; i++ )
+		for( int i = 1; i < 11; i++ ){
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
 			
-			System.out.println(i);
+			
 		
 		
 
